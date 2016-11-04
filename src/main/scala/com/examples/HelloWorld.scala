972ffa84027d599f5f29b0b8644f2db822e26f06
Hovhannes Arzumanyan <hovhannesa@energizeglobal.com>
2016-11-04 13:24:32 +0400

    add HelloWorld samples.

---
+++ src/main/scala/com/examples/HelloWorld.scala
@@ -0,0 +1,5 @@
+package com.examples
+
+object HelloWorld extends App {
+  println("Hello World!")
+}
---
+++ src/main/scala/com/examples/HelloWorld2.scala
@@ -0,0 +1,7 @@
+package com.examples
+
+object HelloWorld2 {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
